883d90a929eb723d8ff2519ecd6c0f90eb871c9e
miheret-eng <mihherettad2007@gmail.com.com>
2022-03-26 06:03:31 -0700

    adv

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,8 +10,9 @@ def number_of_subscribers(subreddit):
     r = requests.get(url, headers=header)
     if r.status_code == 200:
         return r.json().get("data", None).get("subscribers", None)
-    else:        
+    else:
         return 0
+
 
 if __name__ == "__main__":
     pass
